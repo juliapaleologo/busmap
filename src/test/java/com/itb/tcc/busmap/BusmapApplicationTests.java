e3d2b5815e5f5b5ddbaeaeb51c3ee3ac213cfd8f
Julia Almeida <juliadealmeidaalmeida@gmail.com>
2023-08-29 11:55:55 -0300

    ambiente de desenvolvimento

---
+++ src/test/java/com/itb/tcc/busmap/BusmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.busmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
