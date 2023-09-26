3a68094c69219d69494d6d58af2924b11fed3273
Julia Almeida <juliadealmeidaalmeida@gmail.com>
2023-09-26 08:30:32 -0300

    criar o de linha mas deixar o de adm

--- src/main/java/com/itb/tcc/busmap/BusmapApplication.java
+++ src/main/java/com/itb/tcc/busmap/BusmapApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BusmapApplication {
-
+public class BusMapApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BusmapApplication.class, args);
-	}
-
+		SpringApplication.run(BusMapApplication.class, args);
+}
 }
